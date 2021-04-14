f28204d5a325101058882c7e9e6435761541784e
shybal10 <shybalibrahim@gmail.com>
2021-04-14 13:34:34 +0300

    simple counter app

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,5 +1,28 @@
 export default {
     data: {
-        title: 'World'
+        timer: 0,
+        multiplier: 1
+    },
+    increment() {
+        if (this.timer == 0) {
+            this.timer = this.multiplier
+        }else {
+            this.timer = this.timer + this.multiplier
+        }
+    },
+    decrement() {
+        this.timer = this.timer - this.multiplier
+        if (this.timer <= 0) {
+            this.timer = 0
+        }
+    },
+    oneMultiple() {
+        this.multiplier = 1
+    },
+    fiveMultiples() {
+        this.multiplier = 5
+    },
+    tenMultiples() {
+        this.multiplier = 10
     }
 }
